442ddb3841a6da2a143d5bd938e3da6089029f69
Artur Lapaev <artur-lapaev@bk.ru>
2019-07-18 14:30:55 +0300

    add interpolation expression in cmp courses

--- angular-mentoring/src/app/coursesPage/courses/courses.component.ts
+++ angular-mentoring/src/app/coursesPage/courses/courses.component.ts
@@ -6,7 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
-
+  courses = [
+    {
+      time: "1h 28 min",
+      date: "9 Nov, 2018",
+      caption: "Video Course 1. Name tag",
+      content: `Learn about where you can find course descriptions, what information they
+      include, how they work, and details about various components of a course description. Course descriptions
+      report information about a university or college's classes. They're published both in course catalogs that
+      outline degree requirements and in course schedules that contain descriptions for all courses offered during
+      a particular semester.`
+    },
+    {
+      time: "5h 28 min",
+      date: "19 Nov, 2018",
+      caption: "Video Course 1. Name tag",
+      content: `Learn about where you can find course descriptions, what information they
+      include, how they work, and details about various components of a course description. Course descriptions
+      report information about a university or college's classes. They're published both in course catalogs that
+      outline degree requirements and in course schedules that contain descriptions for all courses offered during
+      a particular semester.`
+    },
+    {
+      time: "9h 10 min",
+      date: "30 dec, 2018",
+      caption: "Video Course 1. Name tag",
+      content: `Learn about where you can find course descriptions, what information they
+      include, how they work, and details about various components of a course description. Course descriptions
+      report information about a university or college's classes. They're published both in course catalogs that
+      outline degree requirements and in course schedules that contain descriptions for all courses offered during
+      a particular semester.`
+    },
+  ]
   constructor() { }
 
   ngOnInit() {
