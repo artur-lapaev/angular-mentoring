4911bb65bddad6f689a33c9b16a8bed08fb07c3f
Artur Lapaev <artur-lapaev@bk.ru>
2019-08-20 21:21:44 +0300

    add method for login

--- angular-mentoring/src/app/app.component.ts
+++ angular-mentoring/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'am-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'angular-mentoring';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const isUser = localStorage.getItem('user');
+
+    if (isUser) {
+      this.router.navigate(['/courses']);
+    }
+  }
+
 }
--- angular-mentoring/src/app/login-page/login-page.component.ts
+++ angular-mentoring/src/app/login-page/login-page.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'am-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  userAuthentification(email, password) {
+
+    const options = {
+      userEmail: email,
+      userPassword: password
+    };
+
+    const option = JSON.stringify(options);
+
+    localStorage.setItem('user', option);
   }
 
 }
